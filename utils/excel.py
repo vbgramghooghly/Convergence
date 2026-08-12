454aef2a5d4e86d8e2d50d8693590b8f40a18f6c
vbgramghooghly <nodal.hooghly@gmail.com>
2026-08-12 12:29:59 +0530

    Create excel.py

---
+++ utils/excel.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import io
+
+def dataframe_to_excel(df, sheet_name="Sheet1"):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name=sheet_name)
+    return output.getvalue()
